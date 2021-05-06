143ddf1270f1571de3f5f5b5d7e1b68ac59e35ed
Terhemba-Uta Verem <terhembaverem3@gmail.com>
2021-05-06 21:28:01 +0100

    complete version 1.3

--- includes/navigation.php
+++ includes/navigation.php
@@ -15,7 +15,7 @@
         <div class="row align-items-center">
           
           <div class="col-11 col-xl-2">
-            <h1 class="mb-0"><a href="index.html" class="text-white h2 mb-0">ZIM</a></h1>
+            <h1 class="mb-0"><a href="index.html" class="text-white h2 mb-0" style="font-size:24px;">ZIM</a></h1>
           </div>
           <div class="col-12 col-md-10 d-none d-xl-block">
             <nav class="site-navigation position-relative text-right" role="navigation">
